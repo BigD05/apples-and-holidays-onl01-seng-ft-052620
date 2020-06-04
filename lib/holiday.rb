32b537a8accccacd38cf99dbc705f8f31d62a819
kyle Dornan <kyledornan26@gmail.com>
2020-06-04 09:44:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -24,8 +24,7 @@ a =holiday_hash[season][holiday] = supplies
 end 
 
 def all_winter_holiday_supplies(holiday_hash)
-  
-  
+  # return an array of all of the supplies that are used in the winter season
 
 end
 
